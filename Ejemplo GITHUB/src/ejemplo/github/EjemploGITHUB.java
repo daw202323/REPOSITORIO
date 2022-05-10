fadd9ded71bbff52e2abfca4dd2328829527ec57
Hugo <Hugo@DESKTOP-N0G0RPT>
2022-05-10 13:33:08 +0200

    primer commit

---
+++ Ejemplo GITHUB/src/ejemplo/github/EjemploGITHUB.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejemplo.github;
+
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.Vector;
+
+/**
+ *
+ * @author Hugo
+ */
+public class EjemploGITHUB {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Vector<Integer> numeros = new Vector<Integer>();
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; numeros.size() < 10; i += 1) {
+
+            System.out.println("[" + (i + 1) + "] INTRODUZCA UN NÚMERO: ");
+
+            boolean quit = false;
+
+            while (!quit) {
+
+                try {
+
+                    numeros.add(sc.nextInt());
+
+                    quit = true;
+
+                } catch (InputMismatchException e) {
+
+                    System.out.println("[" + (i + 1) + "] POR FAVOR, INTRODUZCA UN NÚMERO: ");
+
+                    sc.nextLine();
+
+                }
+
+            }
+
+        }
+
+        Collections.sort(numeros);
+
+        for (int numero : numeros) {
+
+            System.out.println(numero);
+
+        }
+
+    }
+
+}
